9cb2effcb3d8abd821fdeea50ca0990a17d43a79
EaterUsr <129983819+EaterUsr@users.noreply.github.com>
2023-11-11 11:42:14 +0100

    build: add postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import postcssNested from "postcss-nested";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   base: "./",
+  css: {
+    postcss: {
+      plugins: [postcssNested, autoprefixer],
+    },
+  },
 });
